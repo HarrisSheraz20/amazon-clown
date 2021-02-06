cae05687189c09bc287eedcc58359d62f48c0f24
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-02-06 12:47:42 +0000

    Now working on home component

--- amazon-clown/src/App.js
+++ amazon-clown/src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 import "./Style/AppMain.css"
 import "./Style/AppGrid.css"
 import Header from "./Components/Header"
+import Home from "./Components/Home"
 
 
 function App() {
   return (
     <div className="app">
       <Header  /> 
-      {/* Home */}
+      <Home />
     </div>
   );
 }
