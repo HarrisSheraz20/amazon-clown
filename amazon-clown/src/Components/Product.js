d0e3a730fa2281b26151cd6b8ff34884361af740
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-02-10 23:20:14 +0000

    more checkout pages

--- amazon-clown/src/Components/Product.js
+++ amazon-clown/src/Components/Product.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import '../Style/AppMain.css'
+import { useStateValue } from './StateProvider';
 
 export const Product = ({ id, title, image, price, rating}) => {
+    const [{ basket },dispatch] = useStateValue();
+    
+    console.log("this is the basket >>>>", basket);
+
+
+    const addToBasket = () =>{
+        // dispatch the item into the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item:{
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            },
+        })
+    }
+
+
     return (
         <div className="product">
             <div className="product__info">
@@ -17,7 +38,7 @@ export const Product = ({ id, title, image, price, rating}) => {
                 </div>
             </div>
             <img src={image} alt=""/>
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
