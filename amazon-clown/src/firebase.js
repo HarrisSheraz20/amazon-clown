74c0250e77e9d446dc559af84caeea25d4a258f2
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-02-13 16:47:00 +0000

    added firebase

---
+++ amazon-clown/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-T9CO8rPo-aeEbSFnRtecXZE87FX5OBI",
+  authDomain: "clown-12d8a.firebaseapp.com",
+  projectId: "clown-12d8a",
+  storageBucket: "clown-12d8a.appspot.com",
+  messagingSenderId: "425599927185",
+  appId: "1:425599927185:web:82286967892f67a7d9d030",
+  measurementId: "G-1S8734TVT9",
+};
